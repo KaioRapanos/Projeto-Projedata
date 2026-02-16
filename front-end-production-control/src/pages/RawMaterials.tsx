0b31b570a35588153069bbabc034b6cfa5afc9cd
Kaio Cezar Rapanos <kaiorapanos@gmail.com>
2026-02-16 09:46:17 -0300

    feat(frontend): create pages and configure application routes

---
+++ front-end-production-control/src/pages/RawMaterials.tsx
@@ -0,0 +1,5 @@
+function RawMaterials() {
+  return <h1>Raw Materials</h1>
+}
+
+export default RawMaterials
---
+++ front-end-production-control/src/routes/AppRoutes.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from '../components/Layout'
+
+import Home from '../pages/Home'
+import Products from '../pages/Products'
+import RawMaterials from '../pages/RawMaterials'
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/raw-materials" element={<RawMaterials />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  )
+}
+
+export default AppRoutes
